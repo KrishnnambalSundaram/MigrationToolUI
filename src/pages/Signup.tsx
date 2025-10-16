a87a7a93adb66a832d800619318a588e0ffa21ed
vmekala-axxel <vmekala@axxeltechnologies.com>
2025-10-16 13:27:26 +0530

    Javelin logo updated

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { AlertCircle } from "lucide-react";
 import { useAuth } from "../context/AuthContext";
 import Background from '../assets/expleo-background.svg';
-import Logo from '../assets/inflecto-logo.svg';
+import Logo from '../assets/Javelin.svg';
 
 const Signup: React.FC = () => {
   const navigate = useNavigate();
